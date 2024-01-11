f8a29939548d0dba9cbc77c41e15e82290124ac1
usamamalik909 <malik.usama9100@gmail.com>
2024-01-11 15:41:06 +0500

    project setup

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '@styles/globals.css'
+import NavBar from '@components/NavBar'
+import Provider from '@components/Provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='main'>
+          <div className='gradient'/>
+        </div>
+        <main className='app'>
+        <NavBar />
+        {children}
+        </main>
+      </body>
     </html>
   )
 }
